ae318ed0197191a401e5bd89620e5c86394c11cd
TimoTF5 <37983685+TimoTF5@users.noreply.github.com>
2018-06-24 00:17:01 +0200

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,205 @@
+<?php
+
+/**
+Open source CAD system for RolePlaying Communities.
+Copyright (C) 2017 Shane Gill
+
+This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+This program comes with ABSOLUTELY NO WARRANTY; Use at your own risk.
+**/
+
+include("./oc-config.php");
+require("./actions/api.php");
+session_start();
+
+if (empty($_SESSION['logged_in']))
+{
+	header('Location: ./index.php');
+    die("Not logged in");
+}
+    setDispatcher("1");
+
+/*
+    The purpose of this page is to simply determine if the user has multiple roles.
+    If they do, provide them the option to go where they want to go.
+    Else, redirect to the only place they can go.
+*/
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$link = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+if (!$link) {
+    die('Could not connect: ' .mysql_error());
+}
+
+$id = $_SESSION['id'];
+$sql = "SELECT * from user_departments WHERE user_id = \"$id\"";
+
+$result=mysqli_query($link, $sql);
+
+$adminButton = "";
+$dispatchButton = "";
+$highwayButton = "";
+$stateButton = "";
+$fireButton = "";
+$emsButton = "";
+$sheriffButton = "";
+$policeButton = "";
+$civilianButton = "";
+
+$num_rows = $result->num_rows;
+// This loop will auto redirect the user if they only have one option
+// TODO: Add the rest of the headers
+if($num_rows < 2)
+{
+    while($row = mysqli_fetch_array($result, MYSQLI_BOTH))
+    {
+        if ($row[1] == "0")
+        {
+            $_SESSION['admin'] = 'YES';
+            header("Location:".BASE_URL."/oc-admin/admin.php");
+
+        }
+        else if ($row[1] == "1")
+        {
+            $_SESSION['dispatch'] = 'YES';
+            header("Location:".BASE_URL."/cad.php");
+
+        }
+        else if ($row[1] == "2")
+        {
+            $_SESSION['ems'] = 'YES';
+            header("Location:".BASE_URL."/mdt.php?dep=fire");
+        }
+            else if ($row[1] == "3")
+        {
+            $_SESSION['fire'] = 'YES';
+            header("Location:".BASE_URL."/mdt.php?dep=fire");
+        }
+        else if ($row[1] == "4")
+        {
+            $_SESSION['highway'] = 'YES';
+            header("Location:".BASE_URL."/mdt.php?dep=police");
+        }
+        else if ($row[1] == "5")
+        {
+            $_SESSION['police'] = 'YES';
+            header("Location:".BASE_URL."/mdt.php?dep=police");
+        }
+        else if ($row[1] == "6")
+        {
+            $_SESSION['sheriff'] = 'YES';
+            header("Location:".BASE_URL."/mdt.php?dep=police");
+        }
+        else if ($row[1] == "7")
+        {
+            $_SESSION['civilian'] = 'YES';
+            header("Location:".BASE_URL."/civilian.php");
+        }
+
+    }
+}
+else
+{
+
+    while($row = mysqli_fetch_array($result, MYSQLI_BOTH))
+    {
+        if ($row[1] == 0)
+        {
+            $_SESSION['admin'] = 'YES';
+						$adminButton = "<a href=\"".BASE_URL."/oc-admin/admin.php\" class=\"btn btn-primary btn-lg\">Admin</a>";
+        }
+        if ($row[1] == 1)
+        {
+            $_SESSION['dispatch'] = 'YES';
+            $dispatchButton = "<a href=\"".BASE_URL."/cad.php\" class=\"btn btn-primary btn-lg\">Dispatch</a>";
+        }
+        if ($row[1] == "2")
+        {
+            $_SESSION['ems'] = 'YES';
+            $emsButton = "<a href=\"".BASE_URL."/mdt.php?dep=fire\" class=\"btn btn-primary btn-lg\">EMS</a>";
+        }
+        if ($row[1] == "3")
+        {
+            $_SESSION['fire'] = 'YES';
+            $fireButton = "<a href=\"".BASE_URL."/mdt.php?dep=fire\" class=\"btn btn-primary btn-lg\">Fire</a>";
+        }
+        if ($row[1] == "4")
+        {
+            $_SESSION['highway'] = 'YES';
+            $highwayButton = "<a href=\"".BASE_URL."/mdt.php?dep=police\" class=\"btn btn-primary btn-lg\">Highway Patrol</a>";
+        }
+        if ($row[1] == "5")
+        {
+            $_SESSION['police'] = 'YES';
+            $policeButton = "<a href=\"".BASE_URL."/mdt.php?dep=police\" class=\"btn btn-primary btn-lg\">Police Department</a>";        }
+        if ($row[1] == "6")
+        {
+            $_SESSION['sheriff'] = 'YES';
+            $sheriffButton = "<a href=\"".BASE_URL."/mdt.php?dep=police\" class=\"btn btn-primary btn-lg\">Sheriff's Office</a>";
+        }
+        if ($row[1] == "9")
+        {
+            $_SESSION['state'] = 'YES';
+            $stateButton = "<a href=\"".BASE_URL."/mdt.php?dep=police\" class=\"btn btn-primary btn-lg\">State Police</a>";
+        }
+        if ($row[1] == "7")
+        {
+            $_SESSION['civillian'] = 'YES';
+            $civilianButton = "<a href=\"".BASE_URL."/civilian.php\" class=\"btn btn-primary btn-lg\">Civilian</a>";
+        }
+    }
+}
+mysqli_close($link);
+
+
+?>
+
+<html lang="en">
+   <!DOCTYPE html>
+   <?php include "./oc-includes/header.inc.php"; ?>
+   <body>
+      <div id="page-wrapper">
+         <div class="container-fluid">
+            <div class="row">
+               <div class="col-lg-12">
+                  <h1 class="page-header" style="text-align:center;">Select console to launch:</h1>
+               </div>
+               <!-- ./ col-lg-12 -->
+            </div>
+            <!-- ./ row -->
+            </div class="row">
+            <div class="col-lg-12" id="cadWelcome">
+               <?php echo $adminButton;?><br/><br/>
+               <?php echo $dispatchButton;?>
+               &nbsp;
+               <?php echo $sheriffButton;?>
+               &nbsp;
+               <?php echo $highwayButton;?>
+               &nbsp;
+               <?php echo $stateButton;?>
+               &nbsp;
+               <?php echo $policeButton;?>
+               &nbsp;
+               <?php echo $fireButton;?>
+			   &nbsp;
+			   <?php echo $emsButton;?>
+               &nbsp;
+               <?php echo $civilianButton;?>
+            </div>
+            <!-- ./ col-lg-12 -->
+         </div>
+         <!-- ./ row -->
+      </div>
+      <!-- ./ container-fluid -->
+      </div>
+      <!-- ./ page-wrapper -->
+   </body>
+</html>
---
+++ oc-config.sample.php
@@ -0,0 +1,178 @@
+<?php
+
+/**#@+
+  *Application Settings
+  *
+  * This section includes settings for Database connectivity, base url, email config, and
+  * These settings are MANDATORY. If they are not configured properly OpenCAD will not function correctly.
+  *
+  * When editing configuration constants be sure to only edit the contents of the second set of quotes in each.
+  * @since 1.0a RC2
+  **/
+
+/**#@+
+ *Community Name
+ *
+ * Set your communities name by changing 'My Community'
+ * @since 1.0a RC1
+ */
+define('COMMUNITY_NAME', 'My Community');
+
+/**#@+
+ * Database connection variables
+ *
+ * @since 1.0a RC1
+ */
+define('DB_NAME', 'openCAD');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', '127.0.0.1');
+
+/**#@+
+ * Base URL Settings
+ *
+ * BASE_URL - The URL to your installation of OpenCAD inlcuding,
+ *             if utilized, it's subdirectory
+ *
+ *             Valid Examples include:
+ *             //example.com - Root domain, no subdirectory
+ *             //subdomain.example.com - subdomain, no subdirectory
+ *             //subdomain.example.comsubdir - subdomain with subdirectory
+ *             //example.com/subdir - root domain with subdirectory
+ *
+ *             The OpenCAD teams does not reccomend including the trailing / on any of the above examples.
+ *             It won't necessarily break anything but just makes reference look strange having
+ *             two slashed when it isn't needed.
+ *
+ * @since 1.0a RC1
+ */
+define('BASE_URL', '//example.com');
+
+/**#@+
+ *To & From emails for system generated emails
+ * To be used in a later version for notificaton emails
+ *
+ * @since 1.0a RC2
+ *
+ */
+define('CAD_FROM_EMAIL', 'cad@community.com');
+define('CAD_FROM_NAME', COMMUNITY_NAME.' CAD');
+define('CAD_TO_EMAIL', 'admins@community.com');
+define('CAD_TO_NAME', COMMUNITY_NAME.' Administrators');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link  WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * UTILIZIATION TO BE IMPLEMENTED
+ *
+ * @since 1.0a RC2
+ *
+ **/
+define('AUTH_KEY',         'put your unique phrase here');
+define('SECURE_AUTH_KEY',  'put your unique phrase here');
+define('LOGGED_IN_KEY',    'put your unique phrase here');
+define('NONCE_KEY',        'put your unique phrase here');
+define('AUTH_SALT',        'put your unique phrase here');
+define('SECURE_AUTH_SALT', 'put your unique phrase here');
+define('LOGGED_IN_SALT',   'put your unique phrase here');
+define('NONCE_SALT',       'put your unique phrase here');
+
+/**#@+
+  * Feature Settings
+  *
+  * These settings effect OpenCAD's core functions
+  * These setting will likely be moved to an *_options table in a future version.
+  *
+  * @since  OpenCAD 1804
+  **/
+
+/**#@+
+ * HIDE_NCIC_RESCUCE
+ *
+ * These settings effect OpenCAD's core functions
+ * These setting will likely be moved to an *_options table in a future version.
+ *
+ * @since  OpenCAD 1804
+**/
+define('HIDE_NCIC_RESCUCE', true);
+
+/**#@+
+ * OpenCAD Feature Settings
+ *
+ * This section controls settings for OpenCAD's core features.
+ * These setting will likely be moved to an *_options table in a future version.
+ *
+ * @since OpenCAD 1804
+ **/
+
+ /**#@+
+  * POLICE_NCIC
+  *
+  * Shows/Hides NCIC functionality on MDT console.
+  * If 'true' then LEO will be able to use NCIC functions without the need for
+  * a dispatcher, else if 'flase' then LEO will require the presence of
+  * dispatcher to use NCIC funcationality.
+  * These settings will likely be moved to an *_options table in a future version.
+  *
+  * @sicne OpenCAD 1804
+  **/
+define('POLICE_NCIC', false);
+
+/**#@+
+ * CIV_WARRANT
+ *
+ * Allow/Disallow Civiliians from managing their warrants. If set to 'true'
+ * then Civs will be able to delete warrants from their profile, else if set
+ * to 'false' then Civs will not have the ability to remove warrants.
+ * These settings will likely be moved to an *_options table in a future version.
+ *
+ * @since  OpenCAD 1803
+ **/
+define('CIV_WARRANT', false);
+
+/**#@+
+ * CIV_REG
+ *
+ * Allow/Disallow direct registration for Civillians.
+ * If set to 'true' then civillians will not require admin approval, esle if
+ * set to 'false' then Civillian registartion will require Admin approval.
+ * Allow/Disallow immediate regitration for civilians.
+ * If 'true' then civilian registartion will require Administrator approval
+ * else if 'false' then civilian registrations will NOT require
+ * Administrator approval.
+ * These settings will likely be moved to an *_options table in a future version.
+ *
+ * @since  OpenCAD 1803
+ **/
+define('CIV_REG', false);
+
+/**#@+
+  * Extra Settings
+  *
+  * This section included boolean settings for Gravatar Fetch, NCIC in MDT, and more.
+  * These setting will likely be moved to an *_options table in a future version.
+  *
+  * @since  1.0a RC2
+  **/
+
+/**#@+
+  * Gravatar Fetch
+  *
+  * OpenCAD will dynamically retrieve your avatar from {@link Gravatar http://en.gravatar.com/} if you have an account. Otherwise
+  * it will use the default generic avatar image included with OpenCAD .
+  *
+  * @since 1.0a RC1
+  **/
+define('USE_GRAVATAR', true);
+
+/** That's all, stop editing! Happy roleplaying. **/
+/**    Absolute path to the OpenCAD directory.   **/
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+include ABSPATH . "oc-functions.php";
+?>
